1666cccb33b549ce169a6bda1c9dc7207044ab92
Minji <minjilee0423@gmail.com>
2020-11-19 17:59:59 +0900

    Added backend

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-  if (!req.user) {
-    // staus code 401 means Unauthorized
-    return res.status(401).send({ error: 'You must log in!' });
+  if (req.user.credits < 1) {
+    // staus code 403 means Forbidden
+    return res.status(403).send({ error: 'Not enough credits!' });
   }
 
   // if user is exist, let's let this user continue on to the actual request handler
---
+++ models/Survey.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
+
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [RecipientSchema],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  // by convention _user means it's supposed to set up a realtionship between this model and another one.
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date
+});
+
+mongoose.model('surveys', surveySchema);
